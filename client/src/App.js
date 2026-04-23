06ceb97d486b74d1cacdcc32e95123651f57967a
sathyachandru35-boop <sathyachandru35@gmail.com>
2026-04-23 13:26:21 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,20 @@
 import React, { useState, useEffect } from "react";
 import ChatPage from "./pages/ChatPage";
-import "./styles/app.css";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  const [rooms, setRooms] = useState({});
-  const [activeRoom, setActiveRoom] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
-    fetch("http://localhost:5000/rooms")
-      .then(res => res.json())
-      .then(data => {
-        const formattedRooms = {};
-
-        data.forEach(room => {
-          formattedRooms[room._id || room.id] = {
-            name: room.name,
-            unread: 0,
-            messages: []
-          };
-        });
-
-        setRooms(formattedRooms);
-        setActiveRoom(data[0]?._id || data[0]?.id);
-      })
-      .catch(err => console.error("Error fetching rooms:", err));
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
+    if (token && user) setLoggedIn(true);
   }, []);
 
-  // 🔥 WAIT until data is ready
-  if (!activeRoom || !rooms[activeRoom]) {
-    return <div style={{ padding: "20px" }}>Loading...</div>;
-  }
-
-  // 🔥 MAIN APP
-  return (
-    <ChatPage
-      rooms={rooms}
-      activeRoom={activeRoom}
-      setActiveRoom={setActiveRoom}
-      setRooms={setRooms}
-    />
+  return loggedIn ? (
+    <ChatPage onLogout={() => setLoggedIn(false)} />
+  ) : (
+    <LoginPage onAuthSuccess={() => setLoggedIn(true)} />
   );
 }
 
